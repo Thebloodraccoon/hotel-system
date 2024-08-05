42d2e7e3a10ac92ec2d110ef3795da9a15e10828
Raccoon <vsafirko@gmail.com>
2024-08-05 22:46:54 +0300

    Start updating test

--- src/test/java/ua/thecoon/hotel/service/HotelServiceTest.java
+++ src/test/java/ua/thecoon/hotel/service/HotelServiceTest.java
@@ -3,33 +3,65 @@ package ua.thecoon.hotel.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import ua.thecoon.hotel.config.ServiceConfig;
+import ua.thecoon.hotel.exception.EntityNotFoundException;
+import ua.thecoon.hotel.model.dto.HotelDTO;
+import ua.thecoon.hotel.model.entity.Hotel;
 import ua.thecoon.hotel.model.mapper.HotelMapper;
 import ua.thecoon.hotel.repo.HotelRepo;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 @ContextConfiguration(classes = {ServiceConfig.class})
-public class HotelServiceTest extends ServiceConfig {
+public class HotelServiceTest {
+
     @Mock
     private HotelRepo hotelRepo;
-    private HotelMapper hotelMapper;
-    private HotelService hotelService;
 
+    @Mock
+    private HotelMapper hotelMapper;
+
+    @InjectMocks
+    private HotelServiceImpl hotelService;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-
-        hotelMapper = getHotelMapper();
-        hotelService = new HotelServiceImpl(hotelRepo, hotelMapper);
     }
 
     @Test
-    public void getAllUserTest_success() {
-        
+    public void getAllHotelsTest_success() {
+
+    }
+
+    @Test
+    public void getHotelTest_success() throws EntityNotFoundException {
+
+    }
+
+    @Test
+    public void saveHotelTest_success() {
+
+    }
+
+    @Test
+    public void updateHotelTest_success() throws EntityNotFoundException {
+
+    }
+
+    @Test
+    public void deleteHotelTest_success() throws EntityNotFoundException {
+
     }
 }
